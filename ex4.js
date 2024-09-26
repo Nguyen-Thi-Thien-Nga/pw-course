0ac9b403f65d114f58e563134d7e4b1e0800dc92
Thien Nga <thiennga12t1@gmail.com>
2024-09-26 16:26:03 +0700

    feat: bai tap section 3

---
+++ ex4.js
@@ -0,0 +1,24 @@
+// Các thông số sức khỏe được tính như sau:
+// Cân nặng lý tưởng = Số lẻ của chiều cao (tính bằng cm) x 9 rồi chia 10 Mức cân tối đa = Bằng số lẻ của chiều cao (tính bằng cm)
+// Mức cân tối thiểu = Số lẻ của chiều cao (tính bằng cm) x 8 rồi chia 10
+// Như vậy, nếu bạn cao 1,7m, tức 170cm thì :
+// Cân cân nặng lý tưởng của bạn là: 70 x 9: 10 = 63 kg Cân nặng tối đa là: 70kg
+// Cân nặng tối thiểu là: 70 x 8 :10 = 56 kg
+// Tạo file ex4.js, thêm vào code đáp án cho đề bài sau:
+// a. Khai báo chiều cao của bạn (cm)
+// b. In ra cân nặng lý tưởng, cân nặng tối đa, cân nặng tối thiểu trên cùng một dòng.
+
+const chieuCao = 155;
+const soLeChieuCao = chieuCao % 100;
+const canNangLyTuong = (soLeChieuCao * 9) / 10;
+const canNangToiDa = soLeChieuCao;
+const canNangToiThieu = (soLeChieuCao * 8) / 10;
+
+console.log(
+  'Cân nặng lý tưởng: ' +
+    canNangLyTuong +
+    ', cân nặng tối đa: ' +
+    canNangToiDa +
+    ', cân nặng tối thiểu: ' +
+    canNangToiThieu
+);
---
+++ ex8.js
@@ -0,0 +1,5 @@
+// Tạo file ex8.js, thêm vào code đáp án cho đề bài sau:
+// a. Incácgiátrịcáchnhau4đơnvịtừ1đến100. i. VD: 1, 5, 9, 13,...
+for (let i = 1; i <= 100; i += 4) {
+  console.log(i);
+}
